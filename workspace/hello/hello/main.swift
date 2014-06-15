5a21bedbe42c00ba79a4ab4b4769a666e023b0b5
tandaly <619606426@qq.com>
2014-06-14 21:44:33 -0700

    init

---
+++ workspace/hello/hello/main.swift
@@ -0,0 +1,28 @@
+//
+//  main.swift
+//  test
+//
+//  Created by tanf on 14-6-14.
+//  Copyright (c) 2014年 tanf. All rights reserved.
+//
+
+import Foundation
+
+//打印语句
+println("Hello, World!")
+
+//定义变量
+var name = "tanf"
+println(name)
+
+//定义常量
+let i = 10
+println(i)
+
+//指定类型
+var a:Int = 20
+println(a)
+
+var b = 40
+var c = a + b
+println(c)
