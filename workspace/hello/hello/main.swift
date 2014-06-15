f87d049f6facb15cd2d1165b13071c42657ec2a3
tandaly <619606426@qq.com>
2014-06-14 22:24:55 -0700

    提交测试代码

--- workspace/hello/hello/main.swift
+++ workspace/hello/hello/main.swift
@@ -23,6 +23,50 @@ println(i)
 var a:Int = 20
 println(a)
 
-var b = 40
-var c = a + b
-println(c)
+//最小值和最大值
+var minInt = Int.min
+var maxInt = Int.max
+println("minInt=\(minInt),maxInt=\(maxInt)")
+
+//数制
+let decimalInteger = 17 //十进制
+let binaryInteger =  0b10001    //二进制
+let octalInteger = 0o21 //八进制
+let hexadecimalInteger = 0x11   //十六进制
+println("decimalInteger=\(decimalInteger),binaryInteger=\(binaryInteger), octalInteger=\(octalInteger), hexadecimalInteger=\(hexadecimalInteger)")
+
+//先声明再赋值
+var str: String
+str = "hello"
+println(str)
+
+//当你要再一行写多个语句时需要加上分号
+println("hello"); println("work")
+
+//可读性
+let a1 = 1_000_000.000_000_1
+println(a1)
+
+//布尔值
+let bool = true
+if bool
+{
+    println(bool)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
